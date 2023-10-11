635c4dbd55a5ee15a663d3a8d6e738265e2d7f53
Joan Kariuki <joan.kariuki01@student.moringaschool.com>
2023-10-11 02:17:37 -0700

    Task 7 - program that prints alphabet in reverse followed by a new line

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints the lowercase alphabet in reverse,
+ *
+ * followed by a new line
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char n;
+
+	for (n = 'z'; n >= 'a'; n--)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
