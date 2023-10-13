ad6f1fde44d015ea073a115d82ac0f0074a25a6d
Joan Kariuki <joan.kariuki01@student.moringaschool.com>
2023-10-13 02:46:45 -0700

    Fixed syntax error

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
  * _isupper - checks is character is uppercase
- *
+ i
  * @c: character to check
  *
- * Return: 1 if check failed, 0 for success
+ * Return: 1 if uppercase, 0 if lowercase
  */
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
+	if ((c >= 'A') && (c <= 'Z'))
 	{
 		return (1);
 	}
