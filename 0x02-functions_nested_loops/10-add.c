087673ff75c697d0b36da3fee42ed92c45552376
Joan Kariuki <joan.kariuki01@student.moringaschool.com>
2023-10-15 03:50:52 -0700

    Final corrections

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - main function
+ *
+ * @a: first integer
+ *
+ * @b: second integer
+ *
+ * Return: sum of a and b
+ */
+int add(int a, int b)
+{
+	return (0);
+}
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * times-table - main function
+ * times_table - prints the 9 times table, starting with 0.
  */
 void times_table(void)
 {
