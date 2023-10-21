0916613eea3b5bcdf004b0b12b94b522ca1d15a5
Joan Kariuki <joan.kariuki01@student.moringaschool.com>
2023-10-21 05:34:12 -0700

    Task 1 correction

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,1 +1,31 @@
+#include "main.h"
 
+/**
+ * _strncat - appends n bytes of the src string to the dest string
+ *
+ * @dest: destination
+ *
+ * @src: source
+ *
+ * @n: number of bytes to append
+ *
+ * Return: char value
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int j;
+
+	for (j = 0; dest[j] != '\0'; j++)
+	{}
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[j + i] = src[i];
+	}
+	dest[j + i] = '\0';
+
+	return (dest);
+
+}
