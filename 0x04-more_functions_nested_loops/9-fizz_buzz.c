e13263b05ae241c5b2e77f6dfc3fccdd9b3e2f32
Joan Kariuki <joan.kariuki01@student.moringaschool.com>
2023-10-21 04:07:38 -0700

    Task 9 correction

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - Takes numbers 1 -100, prints 'Fizz' for numbers that are 
+ * main - Takes numbers 1 -100, prints 'Fizz' for numbers that are
  *
- * multiples of 3 and 'Buzz' for multiples of 5. 
+ * multiples of 3 and 'Buzz' for multiples of 5.
  *
  * Multiples of both 3 and 5 print 'FizzBuzz'.
  *
@@ -14,23 +14,25 @@ int main(void)
 {
 	int i;
 
-	i = 1;
-	while (i <= 100)
+	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
-			printf("FizzBuzz");
-		else if (i % 3 == 0)
-			printf("Fizz");
-		else if (i % 5 == 0)
-			printf("Buzz");
-		else
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			if (i % 3 == 0)
+			{
+				printf("Fizz");
+			}
+			if (i % 5 == 0)
+			{
+				printf("Buzz");
+			}
+		} else
+		{
 			printf("%d", i);
-
+		}
 		if (i != 100)
 			printf(" ");
-		i++;
 	}
 	printf("\n");
-
 	return (0);
 }
