e64c84e08a348bc1fb51d89665b47cca4c76e0b4
Joan Kariuki <joan.kariuki01@student.moringaschool.com>
2023-10-23 19:09:08 +0300

    Add new solution to task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -2,11 +2,11 @@
 
 /**
  * _memcpy - copies n bytes from src to dest
- 
+ *
  * @dest: pointer to destination buffer
-
+ *
  * @src: pointer to source buffer
-
+ *
  * @n: number of bytes to copy
  *
  * Return: pointer to dest
@@ -17,5 +17,5 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	{
 		*(dest + n) = *(src + n);
 	}
-    return (dest);
+	return (dest);
 }
