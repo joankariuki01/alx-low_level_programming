6a35d09ff77aeda1a7bb7dd5a79ba04359ecc7f8
Joan Kariuki <joan.kariuki01@student.moringaschool.com>
2023-10-24 11:52:32 -0700

    Add more changes to main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,7 +1,7 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
-char *memset(char *s, char b, unsigned int n);
+char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
