fa64c1afad915b689516e1e8c78fe7550880f46b
Joan Kariuki <joan.kariuki01@student.moringaschool.com>
2023-10-24 12:00:43 -0700

    Fix betty errors for task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,16 +1,15 @@
 #include "main.h"
 
 /**
- * _memset - Fills the first n bytes of the memory area
- * pointed to by s with the constant byte b
- * 
- * @s: buffer array
- * 
- * @b: constant byte
- * 
- * @n: number of bytes
- * 
- * Return: pointer to the memory area s
+ * _memcpy -  copies n bytes from memory area src to memory area dest
+ *
+ * @src: source
+ *
+ * @dest: destination
+ *
+ * @n: number of bytes to copy
+ *
+ * Return: pointer to dest
 */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
