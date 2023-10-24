3fafe977bc37ce6bc0b829d1ab3e3ea6e927d1f6
Joan Kariuki <joan.kariuki01@student.moringaschool.com>
2023-10-24 09:37:42 -0700

    Add new solution to task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -15,7 +15,12 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-        while (n--)
-                *(s + n) = b;
-        return (s);
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
 }
