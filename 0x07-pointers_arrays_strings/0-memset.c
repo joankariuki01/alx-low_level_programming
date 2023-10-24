511392fb976615ca60fc91267f9fca478bafad62
Joan Kariuki <joan.kariuki01@student.moringaschool.com>
2023-10-24 09:17:31 -0700

    Add changes to main.h

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memset - Fills the first n bytes of the memory area
+ * pointed to by s with the constant byte b
+ * 
+ * @s: buffer array
+ * 
+ * @b: constant byte
+ * 
+ * @n: number of bytes
+ * 
+ * Return: pointer to the memory area s
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+        while (n--)
+                *(s + n) = b;
+        return (s);
+}
+~   
