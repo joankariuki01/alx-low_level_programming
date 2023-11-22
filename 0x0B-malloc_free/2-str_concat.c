f360d3df1c66e4aaf62efe251411e1540bad5bbb
joankariuki01 <contact.joankariuki@gmail.com>
2023-11-21 23:20:36 -0800

    Fix betty errors task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,25 +11,25 @@
  * Return: pointer to concatenated string
  */
 
-char *str_concate(char *s1, char *s2)
+char *str_concat(char *s1, char *s2)
 {
 	char *a;
 
-	int i, j, c ,d;
+	int i, j, c, d;
 
-	if(s1 == NULL)
+	if (s1 == NULL)
 		s1 = "";
-	if(s2 == NULL)
+	if (s2 == NULL)
 		s2 = "";
 
-	for(i = 0; s1[i] != '\0'; i++)
+	for (i = 0; s1[i] != '\0'; i++)
 		;
 	for (j = 0; s2[j] != '\0'; j++)
 		;
 
 	a = malloc((i * sizeof(*s1)) + (j * sizeof(*s2)) + 1);
 	if (a == NULL)
-		return(NULL);
+		return (NULL);
 
 	for (c = 0, d = 0; c < (i + j + 1); c++)
 	{
