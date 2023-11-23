f879f0e30d17fdb874f67461fc3bccfa140641b3
joankariuki01 <contact.joankariuki@gmail.com>
2023-11-23 03:44:33 -0800

    Fix more syntax errors in task 4 solution

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -31,6 +31,10 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (!ptr)
 		return (malloc(new_size));
 
+	ptr1 = malloc(new_size);
+	if (!ptr1)
+		return (NULL);
+
 	old_ptr = ptr;
 
 	if (new_size < old_size)
@@ -45,8 +49,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 			ptr1[i] = old_ptr[i];
 	}
 
-
 	free(ptr);
 	return (ptr1);
 }
-
