eed6aaeabc727bc182362e9897a68838dd468781
joankariuki01 <contact.joankariuki@gmail.com>
2023-12-04 01:40:23 -0800

    Fix syntax error in task2 solution

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -27,7 +27,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		if (str == NULL)
 			printf("(nil)");
 		else
-			print("%s", str);
+			printf("%s", str);
 
 
 		if (index != (n - 1) && separator != NULL)
