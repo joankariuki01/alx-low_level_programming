9feabb36a5f342aa0fa638f16e88cbe363806006
joankariuki01 <contact.joankariuki@gmail.com>
2023-12-14 18:16:53 -0800

    Add 4-clear_bit.c, 5-flip_bits.c, update main.h

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * clear_bit - sets the value of a bit to 0 at a given index
+ *
+ * @n: parameter
+ *
+ * @index: index starting from 0 of the bit to set
+ *
+ * Return: 1 if successful, -1 if error
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > sizeof(n) * 8)
+		return (-1);
+	*n &= ~(1 << index);
+	return (1);
+}
